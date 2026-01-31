ba0659b636266673de20e6c081760df926f305a8
Pranav-Pudi <pranav.pudi1@gmail.com>
2026-01-31 22:44:06 +0800

    feat: add greeting message and bye command exit for Level-0

--- src/main/java/PranavBot.java
+++ src/main/java/PranavBot.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * The main entry point for PranavBot, a simple task management chatbot.
+ */
 public class PranavBot {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm PranavBot.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
