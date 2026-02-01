0be4a2a3d71d820dd3492a14a79f07f3e7d666b4
Pranav-Pudi <pranav.pudi1@gmail.com>
2026-02-01 11:40:37 +0800

    feat: implement Todo task type, Deadline task type with due date, Event task type with time range

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,20 @@
-public class Event {
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " to " + to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
 }
